0c541b5d1a99a62f668645237e87df45933cac15
André Silva <dev01766@proton.me>
2023-11-17 14:03:21 +0100

    netlify config

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,8 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./vue-byte5/src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
+  base: '/vue-byte5/'
 })
