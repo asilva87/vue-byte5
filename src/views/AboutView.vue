cd5f72caf4e606af74b37309f2377bfc034dbce9
André Silva <dev01766@proton.me>
2023-11-17 12:15:49 +0100

    Complete About Me

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,7 +1,23 @@
 <template>
     <main>
-        <div>about view</div>
+        <div class="about-me">
+            <p>I’m a programmer from Brazil, having started my career as a programmer in Russia. Even though my Bachelor’s degree is in Business Administration, I rediscovered my love for programming back in 2017 and have been self-studying ever since.</p>
+            <p>At my first job, I single-handedly created a massive platform from scratch using the skills mentioned below. I also maintained and added new features to the previous platform before the release of the new one.</p>
+            <p>And at my previous job I was responsible for maintaining and upgrading a massive CRM platform, as well as writing full-stack applications.</p>
+            <p>Learning is something I’m really passionate about, and I thrive in collaborative work environments. I’m highly motivated, especially when I feel that my contributions are valued and appreciated by the company.</p>
+        </div>
     </main>
 </template>
 
-<style></style>
+<style>
+    .about-me {
+        font-family: 'AvenirNext';
+        margin: 0 5rem;
+    }
+
+    @media screen and (max-width: 740px) {
+    .about-me {
+        margin: 0 1rem; 
+    }
+}
+</style>
