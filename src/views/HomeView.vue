cca76a94699ba4bb6ce981b698e057799da20cfa
André Silva <dev01766@proton.me>
2023-11-19 23:09:07 +0100

    Correct text

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,25 +1,45 @@
 <template>
     <main>
         <div class="page-description">
-            <p>This is a simple SPA I wrote to show at the interview with byte5. It's mean to show my enthusiasm for the Frontend position I'm applying for, and to show that I'm motivated, proactive, and can learn quickly.</p>
-            <p>I learnt Vue only yesterday, but due to my experience with React and Angular, I can see where Vue took inspiration from and quickly understand how to do things.</p>
-            <p>This app is not meant to show best practices, the cleanest code, or the best programming logic or styling. It was made purely with two ideas in mind: show my enthusiasm, start learning Vue.</p>
-            <p>I'm super excited about this position, as I believe the ethos of the company, as I could read in the job description, the company's page as well as in the videos on the Youtube channel super reverberate with me.</p>
-            <p>I don't usually make a whole application for each job interview, but for byte5 it feels different, and that's why I made this simple SPA.</p>
+            <p>
+                This is a simple SPA I wrote to show at the interview with byte5. It's meant to show
+                my enthusiasm for the Frontend position I'm applying for, and to show that I'm
+                motivated, proactive, and that I can learn quickly.
+            </p>
+            <p>
+                I learnt Vue only yesterday (16.11 - Interview was on 17.11), but due to my
+                experience with React and Angular, I can see where Vue took inspiration from and can
+                quickly understand how to do things.
+            </p>
+            <p>
+                This app is not meant to show best practices, the cleanest code, or the best
+                programming logic or styling. It was made purely with two ideas in mind: show my
+                enthusiasm, start learning Vue.
+            </p>
+            <p>
+                I'm super excited about this position, as I believe the ethos of the company (as I
+                could read in the job description, the company's page, as well as in the videos on
+                the Youtube channel) super reverberate with me.
+            </p>
+            <p>
+                I don't usually make a whole application for each job interview, but for
+                <strong>byte5</strong> it felt different, and that's why I made this simple SPA.
+            </p>
             <p>Thanks for visiting. =)</p>
         </div>
     </main>
 </template>
 
 <style>
-    .page-description {
-        font-family: 'AvenirNext';
-        margin: 0 5rem;
-    }
+.page-description {
+    font-family: 'AvenirNext';
+    margin: 0 5rem;
+    text-shadow: 2px 2px 2px white;
+}
 
-    @media screen and (max-width: 740px) {
+@media screen and (max-width: 740px) {
     .page-description {
-        margin: 0 1rem; 
+        margin: 0 1rem;
     }
 }
 </style>
